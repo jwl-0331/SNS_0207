7c703518d9956bd00728bf4b719e3b372ec40e7a
jwl-0331 <109019896+jwl-0331@users.noreply.github.com>
2023-02-08 11:11:42 +0900

    [이재원] -SNS 프로젝트 최초 파일 추가

---
+++ src/main/java/com/jwl/sns/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.jwl.sns.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan(basePackages="com.jwl.sns.*")
+public class DatabaseConfig {
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+		
+		sessionFactory.setMapperLocations(res);
+		
+		return sessionFactory.getObject();
+	}
+}
